da5f6463b6215309b770dd03d3f7d47bc112ff5d
JP <pablopablo179@gmail.com>
2026-03-02 21:03:01 +0000

    feat(analytics): add @vercel/speed-insights/react to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Index from "./pages/Index";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const App = () => (
   <>
     <Index />
     <Analytics />
+    <SpeedInsights />
   </>
 );
 
